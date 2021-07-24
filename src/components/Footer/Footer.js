7834e07f0f1b53e8058a247d81ef531145da8071
Endrial <endrialmp@gmail.com>
2021-07-24 10:27:09 +0700

    fix style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import "../../components/Footer/footer.css";
 class Footer extends Component {
   render() {
     return (
-      <Navbar color="dark" dark expand="md" className="navigation">
+      <Navbar color="dark" dark expand="md" className="footer">
         <Container>
           <p className="desc">
             See <a href="https://github.com/tolend95/la-cinema">Repository</a>
